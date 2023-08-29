abf40f2f5508176a0f868ee30a18ee47fac3c4cc
github_shripad99 <shripadtirlotkar15@gmail.com>
2023-08-29 19:44:34 +0530

    initial commit

---
+++ src/Components/Board/Board.js
@@ -0,0 +1,90 @@
+import './Board.css'
+import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd'
+import mockData from '../../data'
+import { useState } from 'react'
+import Card from '../Card/Card'
+
+const Kanban = () => {
+    const [data, setData] = useState(mockData)
+
+    const onDragEnd = result => {
+        if (!result.destination) return
+        const { source, destination } = result
+
+        if (source.droppableId !== destination.droppableId) {
+            const sourceColIndex = data.findIndex(e => e.id === source.droppableId)
+            const destinationColIndex = data.findIndex(e => e.id === destination.droppableId)
+
+            const sourceCol = data[sourceColIndex]
+            const destinationCol = data[destinationColIndex]
+
+            const sourceTask = [...sourceCol.tasks]
+            const destinationTask = [...destinationCol.tasks]
+
+            const [removed] = sourceTask.splice(source.index, 1)
+            destinationTask.splice(destination.index, 0, removed)
+
+            data[sourceColIndex].tasks = sourceTask
+            data[destinationColIndex].tasks = destinationTask
+
+            setData(data)
+        }
+    }
+
+    return (
+        <DragDropContext onDragEnd={onDragEnd}>
+            <div className="board">
+                {
+                    data.map(section => (
+                        <Droppable
+                            key={section.id}
+                            droppableId={section.id}
+                        >
+                            {(provided) => (
+                                <div
+                                    {...provided.droppableProps}
+                                    className='board-section'
+                                    ref={provided.innerRef}
+                                >
+                                    <div className="board-section-title">
+                                        {section.title}
+                                    </div>
+                                    <div className="board-section-content">
+                                        {
+                                            section.tasks.map((task, index) => (
+                                                <Draggable
+                                                    key={task.id}
+                                                    draggableId={task.id}
+                                                    index={index}
+                                                >
+                                                    {(provided, snapshot) => (
+                                                        <div
+                                                            ref={provided.innerRef}
+                                                            {...provided.draggableProps}
+                                                            {...provided.dragHandleProps}
+                                                            style={{
+                                                                ...provided.draggableProps.style,
+                                                                opacity: snapshot.isDragging ? '0.5' : '1'
+                                                            }}
+                                                        >
+                                                            <Card>
+                                                                {task.title}
+                                                            </Card>
+                                                        </div>
+                                                    )}
+                                                </Draggable>
+                                            ))
+                                        }
+                                        {provided.placeholder}
+                                    </div>
+                                </div>
+                            )}
+                        </Droppable>
+                    ))
+                }
+            </div>
+        </DragDropContext>
+    )
+}
+
+export default Kanban
